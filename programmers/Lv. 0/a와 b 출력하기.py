94392acdd7b7292c101eb946377481c02e7fc759
etesong014 <etesong0114@gmail.com>
2023-09-18 21:45:41 +0900

    프로그래머스 코딩테스트 Lv. 0 - Lv. 1에 어려움을 느끼고 0부터 푸는 것으로 변경 - map, upper, lower 등

---
+++ programmers/Lv. 0/a와 b 출력하기.py
@@ -0,0 +1,22 @@
+'''
+문제 설명
+정수 a와 b가 주어집니다. 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
+
+입력 #1
+4 5
+
+출력 #1
+a = 4
+b = 5
+'''
+
+a, b = map(int, input().strip().split(' '))
+print(f'a = {a} \nb = {b}')
+
+
+
+
+'''
+map 함수
+https://dojang.io/mod/page/view.php?id=2286
+'''
