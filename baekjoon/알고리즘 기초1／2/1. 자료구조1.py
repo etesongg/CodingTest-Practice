a1d2cc45fa0c1a700b2daa1af1715ecfd1e05faf
etesong014 <etesong0114@gmail.com>
2024-02-04 22:06:59 +0900

    백준 자료구조1 - flag 사용

--- baekjoon/알고리즘 기초1／2/1. 자료구조1.py
+++ baekjoon/알고리즘 기초1／2/1. 자료구조1.py
@@ -70,8 +70,35 @@ for _ in range(N):
         else: 
             print("NO")
 
-        
+# 1874
+# 1부터 n까지의 수를 스택에 넣었다가 뽑아 늘어놓음으로써, 하나의 수열을 만들 수 있다. 이때, 스택에 push하는 순서는 반드시 오름차순을 지키도록 한다고 하자. 임의의 수열이 주어졌을 때 스택을 이용해 그 수열을 만들 수 있는지 없는지, 있다면 어떤 순서로 push와 pop 연산을 수행해야 하는지를 알아낼 수 있다. 이를 계산하는 프로그램을 작성하라.
+import sys
+input = sys.stdin.readline 
 
+N = int(input())
+stack = []
+answer = []
+flag = 0
+cur = 1
+
+for _ in range(N):
+    num = int(input())
+    while cur <= num:
+        stack.append(cur)
+        answer.append('+')
+        cur += 1
+    
+    if stack[-1] == num:
+        stack.pop()
+        answer.append('-')
+    else:
+        print('NO')
+        flag = 1
+        break
+
+if flag == 0:
+    for i in answer:
+        print(i)
 
 
 
