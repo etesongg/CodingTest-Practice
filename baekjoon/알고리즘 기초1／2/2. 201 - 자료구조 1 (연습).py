fc2435e0e1796b7002d50c46a7d4747f3c5d57f3
etesong014 <etesong0114@gmail.com>
2024-02-08 22:43:53 +0900

    백준 201 자료구조1(연습) - 10799 쇠막대기(9012 참고)

--- baekjoon/알고리즘 기초1／2/2. 201 - 자료구조 1 (연습).py
+++ baekjoon/알고리즘 기초1／2/2. 201 - 자료구조 1 (연습).py
@@ -1,4 +1,4 @@
-# 단어 뒤집기2 17413
+# 17413 단어 뒤집기2 
 import sys
 input = sys.stdin.readline
 
@@ -8,24 +8,24 @@ flag = 0
 res = ''
 
 for word in words:
-    if word == '<':
+    if word == "<":
         flag = 1
         for _ in range(len(stack)):
             res += stack.pop()
     stack.append(word)
     
-    if word == '>':
+    if word == ">":
         flag = 0
         for _ in range(len(stack)):
             res += stack.pop(0)
      
-    if word == ' ' and flag == 0:
+    if word == " " and flag == 0:
         for i in range(len(stack)):
             if i == 0:
                 stack.pop()
                 continue
             res += stack.pop()
-        res += ' '
+        res += " "
 
 if stack:
     for _ in range(len(stack)):
@@ -33,5 +33,23 @@ if stack:
             
 print(res)
 
+# 10799 쇠막대기
+import sys
+input = sys.stdin.readline
 
-            
+laser = input().strip()
+stack = []
+cnt = 0
+
+for i in range(len(laser)):
+    if laser[i] == "(":
+        stack.append("(")
+    else:
+        if i > 0 and laser[i-1] == "(":
+            stack.pop()
+            cnt += len(stack)
+        else:
+            stack.pop()
+            cnt += 1
+
+print(cnt)            
